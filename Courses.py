2fe10255bcc5d36fde4fbd895262e09d50c755f5
Jay23Cee <41491633+Jay23Cee@users.noreply.github.com>
2019-08-11 20:18:30 -0700

    + a student clasds that inheritance of a Person

--- Courses.py
+++ Courses.py
@@ -1,13 +1,39 @@
-courses = { "Math":["Algebra","Geometry", "Statistic", "Trig"],"Co Sci" :["Python", "Java", "C++"] , "Science" : ["Biology", "Chemistry", "MicroBiology"]     }
+courses = { "Math":["Algebra","Geometry", "Statistic", "Trig"],"Co Sci" :["Python", "Java", "C++"]      }
 
+
+class Person:
+	def __init__(self, name, age, address, gender):
+		self.name = name
+		self.age = age
+		self.address = address
+		self.gender  = gender
+
+class student(Person)  :
+	def __init__( self, name, age, address, gender ):
+		self.id = self.getId()
+		super().__init__(name,age,address,gender)
+#		Person.name= name
+#		Person.age= age
+#		Person.address= address
+#		Person.gender= gender
+		 
+	
+	def getId(self):
+		return 0
+		
+	def toString(self):
+		stri = self.name + self.address
+		return stri
+		
 
 
 def getClass(mclass):
 		if mclass in courses:
 			for cclass  in courses[mclass]:
 					print(cclass)
-		
           
 
 
-getClass("Math")
+#getClass("Math")
+s1 = student("james", 22, "123 fake st.", "Male")
+print (s1.toString())
